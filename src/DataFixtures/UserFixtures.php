790de9a05b45cfead47142a591e165599f08e240
TBoileau <t-boileau@email.com>
2021-11-03 15:49:50 +0100

    feat: update fixtures

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -6,7 +6,8 @@ namespace App\DataFixtures;
 
 use App\Entity\Designer;
 use App\Entity\Developer;
-use App\Entity\User;
+use App\Entity\GraphicStyle;
+use App\Entity\Level;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
@@ -19,15 +20,46 @@ final class UserFixtures extends Fixture
 
     public function load(ObjectManager $manager): void
     {
+        $manager->persist($this->createDeveloper());
+        $manager->persist($this->createDesigner());
+        $manager->flush();
+    }
+
+    private function createDeveloper(): Developer
+    {
         $developer = new Developer();
+        $developer->setFirstName('John');
+        $developer->setLastName('Doe');
         $developer->setEmail('developer+1@email.com');
         $developer->setPassword($this->userPasswordHasher->hashPassword($developer, 'password'));
-        $manager->persist($developer);
 
+        /** @var Level $level */
+        $level = $this->getReference('junior');
+        $developer->setLevel($level);
+
+        /** @var GraphicStyle $graphicStyle */
+        $graphicStyle = $this->getReference('flat');
+        $developer->setGraphicStyle($graphicStyle);
+
+        return $developer;
+    }
+
+    private function createDesigner(): Designer
+    {
         $designer = new Designer();
+        $designer->setFirstName('Jane');
+        $designer->setLastName('Doe');
         $designer->setEmail('designer+1@email.com');
         $designer->setPassword($this->userPasswordHasher->hashPassword($designer, 'password'));
-        $manager->persist($designer);
-        $manager->flush();
+
+        /** @var Level $level */
+        $level = $this->getReference('confirmed');
+        $designer->setLevel($level);
+
+        /** @var GraphicStyle $graphicStyle */
+        $graphicStyle = $this->getReference('retro');
+        $designer->setGraphicStyle($graphicStyle);
+
+        return $designer;
     }
 }
