3061d8faefd05db8845f8137bd0fd4b9549a2b01
TBoileau <t-boileau@email.com>
2021-11-03 17:48:40 +0100

    test: create graphic style (#60)

--- tests/Functional/Admin/GraphicStyleTest.php
+++ tests/Functional/Admin/GraphicStyleTest.php
@@ -99,4 +99,37 @@ final class GraphicStyleTest extends WebTestCase
 
         $this->assertEquals('Modifié', $graphicStyle->getName());
     }
+
+    public function testIfGraphicStyleIsCreated(): void
+    {
+        $client = static::createClient();
+
+        /** @var EntityManagerInterface $entityManager */
+        $entityManager = $client->getContainer()->get('doctrine.orm.entity_manager');
+
+        /** @var AdminUrlGenerator $adminUrlGenerator */
+        $adminUrlGenerator = $client->getContainer()->get(AdminUrlGenerator::class);
+
+        $client->loginUser($entityManager->find(Administrator::class, 1), 'admin');
+
+        $client->request(
+            'GET',
+            $adminUrlGenerator
+                ->setController(GraphicStyleCrudController::class)
+                ->setAction(Action::NEW)
+                ->generateUrl()
+        );
+
+        $client->submitForm('Créer', [
+            'GraphicStyle[name]' => 'Nouveau'
+        ]);
+
+        $this->assertResponseRedirects();
+
+        $client->followRedirect();
+
+        $graphicStyle = $entityManager->find(GraphicStyle::class, 3);
+
+        $this->assertEquals('Nouveau', $graphicStyle->getName());
+    }
 }
