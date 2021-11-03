e7aa39a7fa5c72d592e6b1ab9e4b488919ae6739
Thomas Boileau <t-boileau@email.com>
2021-11-03 17:20:55 +0100

    Administrator (#75)
    * build: install easycorp/easyadmin-bundle (#38)
    
    * feat: implement Administrator entity (#39)
    
    * feat: authentication (#73)
    
    * test: admin authentication (#74)

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -5,19 +5,14 @@ declare(strict_types=1);
 namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiProperty;
-use Doctrine\ORM\Mapping\Column;
 use Doctrine\ORM\Mapping\DiscriminatorColumn;
 use Doctrine\ORM\Mapping\DiscriminatorMap;
 use Doctrine\ORM\Mapping\Entity;
-use Doctrine\ORM\Mapping\GeneratedValue;
-use Doctrine\ORM\Mapping\Id;
 use Doctrine\ORM\Mapping\InheritanceType;
 use Doctrine\ORM\Mapping\JoinColumn;
 use Doctrine\ORM\Mapping\ManyToOne;
 use Doctrine\ORM\Mapping\Table;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
-use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
-use Symfony\Component\Security\Core\User\UserInterface;
 
 #[Entity]
 #[InheritanceType('SINGLE_TABLE')]
@@ -25,25 +20,8 @@ use Symfony\Component\Security\Core\User\UserInterface;
 #[DiscriminatorMap(['designer' => Designer::class, 'developer' => Developer::class])]
 #[Table(name: '`user`')]
 #[UniqueEntity(fields: 'email')]
-abstract class User implements UserInterface, PasswordAuthenticatedUserInterface
+abstract class User extends BaseUser
 {
-    #[Id]
-    #[Column(type: 'integer')]
-    #[GeneratedValue]
-    protected ?int $id = null;
-
-    #[Column(unique: true)]
-    protected string $email;
-
-    #[Column]
-    protected string $password;
-
-    #[Column]
-    protected string $firstName;
-
-    #[Column]
-    protected string $lastName;
-
     #[ManyToOne(targetEntity: Level::class)]
     #[JoinColumn(nullable: false)]
     #[ApiProperty(readableLink: false, writableLink: false)]
@@ -53,51 +31,6 @@ abstract class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[JoinColumn(nullable: false)]
     #[ApiProperty(readableLink: false, writableLink: false)]
     protected GraphicStyle $graphicStyle;
-
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
-
-    public function getEmail(): string
-    {
-        return $this->email;
-    }
-
-    public function setEmail(string $email): void
-    {
-        $this->email = $email;
-    }
-
-    public function getPassword(): string
-    {
-        return $this->password;
-    }
-
-    public function setPassword(string $password): void
-    {
-        $this->password = $password;
-    }
-
-    public function getFirstName(): string
-    {
-        return $this->firstName;
-    }
-
-    public function setFirstName(string $firstName): void
-    {
-        $this->firstName = $firstName;
-    }
-
-    public function getLastName(): string
-    {
-        return $this->lastName;
-    }
-
-    public function setLastName(string $lastName): void
-    {
-        $this->lastName = $lastName;
-    }
 
     public function getLevel(): Level
     {
@@ -125,19 +58,5 @@ abstract class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function getRoles(): array
     {
         return ['ROLE_USER'];
-    }
-
-    public function getSalt(): ?string
-    {
-        return null;
-    }
-
-    public function eraseCredentials(): void
-    {
-    }
-
-    public function getUsername(): string
-    {
-        return $this->email;
     }
 }
