04fbd8d584f210473b2a1e6db05704fd938c2993
TBoileau <t-boileau@email.com>
2021-11-03 15:40:00 +0100

    feat: implement GraphicStyle entity (#52)

---
+++ src/Entity/GraphicStyle.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\Id;
+
+#[Entity]
+class GraphicStyle
+{
+    #[Id]
+    #[Column(type: 'integer')]
+    #[GeneratedValue]
+    protected ?int $id = null;
+
+    #[Column]
+    protected string $name;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+}
