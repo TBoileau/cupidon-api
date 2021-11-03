0fb80b58bd201dc8bf8fa8ef789dc78f3d255e65
TBoileau <t-boileau@email.com>
2021-11-03 17:45:03 +0100

    test: show graphic style (#58)

--- tests/Functional/Admin/GraphicStyleTest.php
+++ tests/Functional/Admin/GraphicStyleTest.php
@@ -37,4 +37,31 @@ final class GraphicStyleTest extends WebTestCase
 
         $this->assertCount(2, $crawler->filter('article.content table tbody tr'));
     }
+
+    public function testIfGraphicStyleIsShown(): void
+    {
+        $client = static::createClient();
+
+        /** @var EntityManagerInterface $entityManager */
+        $entityManager = $client->getContainer()->get('doctrine.orm.entity_manager');
+
+        /** @var AdminUrlGenerator $adminUrlGenerator */
+        $adminUrlGenerator = $client->getContainer()->get(AdminUrlGenerator::class);
+
+        $client->loginUser($entityManager->find(Administrator::class, 1), 'admin');
+
+        $crawler = $client->request(
+            'GET',
+            $adminUrlGenerator
+                ->setController(GraphicStyleCrudController::class)
+                ->setAction(Action::DETAIL)
+                ->setEntityId(1)
+                ->generateUrl()
+        );
+
+        $this->assertResponseIsSuccessful();
+
+        $this->assertStringContainsString('1', $crawler->filter('dl.datalist div:first-child dd')->text());
+        $this->assertStringContainsString('Flat design', $crawler->filter('dl.datalist div:last-child dd')->text());
+    }
 }
