b091b515a068727c8b32fe01a9b259921009e5dc
TBoileau <t-boileau@email.com>
2021-11-03 15:26:07 +0100

    feat: update fixtures

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\DataFixtures;
 
+use App\Entity\Designer;
+use App\Entity\Developer;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -17,10 +19,15 @@ final class UserFixtures extends Fixture
 
     public function load(ObjectManager $manager): void
     {
-        $user = new User();
-        $user->setEmail('developer+1@email.com');
-        $user->setPassword($this->userPasswordHasher->hashPassword($user, 'password'));
-        $manager->persist($user);
+        $developer = new Developer();
+        $developer->setEmail('developer+1@email.com');
+        $developer->setPassword($this->userPasswordHasher->hashPassword($developer, 'password'));
+        $manager->persist($developer);
+
+        $designer = new Designer();
+        $designer->setEmail('designer+1@email.com');
+        $designer->setPassword($this->userPasswordHasher->hashPassword($designer, 'password'));
+        $manager->persist($designer);
         $manager->flush();
     }
 }
