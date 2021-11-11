977d4cb2945f5294127c21ea634ae2ddade625af
TBoileau <t-boileau@email.com>
2021-11-11 10:34:51 +0100

    feat: update graphic style fixtures (#95)

--- src/DataFixtures/GraphicStyleFixtures.php
+++ src/DataFixtures/GraphicStyleFixtures.php
@@ -7,21 +7,30 @@ namespace App\DataFixtures;
 use App\Entity\GraphicStyle;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use function Symfony\Component\String\u;
 
 final class GraphicStyleFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $flat = new GraphicStyle();
-        $flat->setName('Flat design');
-        $manager->persist($flat);
-        $this->addReference('flat', $flat);
-
-        $retro = new GraphicStyle();
-        $retro->setName('Retro');
-        $manager->persist($retro);
-        $this->addReference('retro', $retro);
+        $manager->persist($this->createGraphicStyle('Flat'));
+        $manager->persist($this->createGraphicStyle('Retro'));
 
         $manager->flush();
+    }
+
+    private function createGraphicStyle(string $name): GraphicStyle
+    {
+        $slug = u($name)->lower()->toString();
+        copy(
+            __DIR__.'/../../public/uploads/graphic_styles/image.png',
+            sprintf(__DIR__.'/../../public/uploads/graphic_styles/%s.png', $slug)
+        );
+        $graphicStyle = new GraphicStyle();
+        $graphicStyle->setName('Retro');
+        $graphicStyle->setFilename(sprintf('%s.png', $slug));
+        $this->addReference($slug, $graphicStyle);
+
+        return $graphicStyle;
     }
 }
